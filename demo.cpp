a4a3076e69a7bf43e9fd46a0c68b0b02485a1b94
kha akai <nhkhacntt16@gmail.com>
2023-04-03 20:41:52 +0700

    sua code demo

--- demo.cpp
+++ demo.cpp
@@ -3,7 +3,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	std::cout << "hello" << std::endl;
+	std::cout << "helo" << std::endl;
+	std::cout << "hello - 2" << std::endl;
 	
 	return 0;
 }
